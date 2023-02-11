47899b008210ba6a553099a3a5458e031f079c58
fluk <satapornsanla36@gmail.com>
2023-02-11 12:59:59 +0700

    edit login

--- src/app/Login/login-page.component.ts
+++ src/app/Login/login-page.component.ts
@@ -1,8 +1,9 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import { MainService } from '../service/main.service';
 
 @Component({
   selector: 'app-login-page',
@@ -19,7 +20,8 @@ export class LoginPageComponent implements OnInit{
     protected router: Router, 
     protected route: ActivatedRoute,
     private confirmationService: ConfirmationService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private service : MainService,
     ) {
   }
 
@@ -29,37 +31,56 @@ export class LoginPageComponent implements OnInit{
 
   initMainForm(){
     this.formModel = new FormGroup({
-      userName: new FormControl(),
-      password: new FormControl(),
+      username: new FormControl(null,Validators.required),
+      password: new FormControl(null,Validators.required),
     });
     this.formModel.markAllAsTouched();
   }
 
   clickLogin(){
-    this.userId = 10;
-    this.confirmationService.confirm({
-      message: 'คุณกำลังเข้าสู่ระบบสมาชิก',
-      header: 'เข้าสู่ระบบ',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
+    if(this.formModel.valid){
+      const payload = this.formModel.getRawValue();
+      this.service.login(payload).subscribe((res) => {
+        console.log(res,'===================> res login')
+        if(res !== null){
+          this.userId = res.data.id;
           setTimeout(() => {}, 500);
           this.router.navigate(['/main/main-page'],{
             state: {data: this.userId}
           });
-        
-      },
-      reject: () => {
-          this.messageService.add({severity:'error', summary: 'Error', detail: 'เข้าสู๋ระบบไม่สำเร็จ'});
-      }
-    });
+        }else{
+          // this.confirmationService.confirm({
+          //   message: 'เลขสมาชิกเเละรหัสผ่านไม่ถูกต้อง',
+          //   header: 'เข้าสู่ระบบ',
+          //   icon: 'pi pi-exclamation-triangle',
+          //   accept: () => {
+          //   },
+          //   reject: () => {
+          //   }
+          // });
+          this.messageService.add({severity:'error', summary: 'Error', detail: 'เลขสมาชิกเเละรหัสผ่านไม่ถูกต้อง'});
+        }
+      })
+    }else{
+        this.confirmationService.confirm({
+          message: 'กรุณากรอกเลขสมาชิกเเละรหัสผ่าน',
+          header: 'เข้าสู่ระบบ',
+          icon: 'pi pi-exclamation-triangle',
+          accept: () => {
+          },
+          reject: () => {
+          }
+        });
+    }
+    
   }
 
   clickRegister(){
-    this.userId = 10;
     this.router.navigate(['register'],{
-      state: {data: this.userId}
+      state: {data: null}
     });
   }
 
   clickForgetPassWord(){}
+
 }
