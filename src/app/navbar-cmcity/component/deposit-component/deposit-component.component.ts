d3fc41614a9ae62e114bceec41c7e0dfb944c969
Pele <srujirut@gmail.com>
2024-05-13 14:16:46 +0700

    Fix (doc)

--- src/app/navbar-cmcity/component/deposit-component/deposit-component.component.ts
+++ src/app/navbar-cmcity/component/deposit-component/deposit-component.component.ts
@@ -4,26 +4,47 @@ import { saveAs } from 'file-saver';
 @Component({
   selector: 'app-deposit-component',
   templateUrl: './deposit-component.component.html',
-  styleUrls: ['./deposit-component.component.scss']
+  styleUrls: ['./deposit-component.component.scss'],
 })
 export class DepositComponentComponent implements OnInit {
   @ViewChild('downloadLink') downloadLinkRef!: ElementRef;
 
   documents = [
-    { label: 'คู่มือปฏิบัติงานกองทุนฯ', path: '/assets/pdf/คู่มือปฏิบัติงานกองทุนฯ.pdf' },
-    { label: 'ใบสมัครเข้าเป็นสมาชิก', path: '/assets/pdf/ใบสมัครเข้าเป็นสมาชิก.pdf' },
+    {
+      label: 'คู่มือปฏิบัติงานกองทุนฯ',
+      path: '/assets/pdf/คู่มือปฏิบัติงานกองทุนฯ.pdf',
+    },
+    {
+      label: 'ใบสมัครเข้าเป็นสมาชิก',
+      path: '/assets/pdf/ใบสมัครเข้าเป็นสมาชิก.pdf',
+    },
     { label: 'หนังสือขอลาออก', path: '/assets/pdf/หนังสือขอลาออก.pdf' },
-    { label: 'หนังสือสัญญากู้กองทุนสวัสดิการ', path: '/assets/pdf/หนังสือสัญญากู้กองทุนสวัสดิการ.pdf' },
-    { label: 'หนังสือค้ำประกันเงินกู้กองทุนสวัสดิการ', path: '/assets/pdf/หนังสือค้ำประกันเงินกู้กองทุนสวัสดิการ.pdf' },
-    { label: 'หนังสือขอเปลี่ยนแปลงการใช้หุ้นค้ำประกัน', path: '/assets/pdf/หนังสือขอเปลี่ยนแปลงการใช้หุ้นค้ำประกัน.pdf' },
-    { label: 'หนังสือขอเปลี่ยนผู้ค้ำประกัน', path: '/assets/pdf/หนังสือขอเปลี่ยนผู้ค้ำประกัน.pdf' },
-    { label: 'หนังสือขอรับเงินช่วยสมาชิกที่เกษียณ', path: '/assets/pdf/หนังสือขอรับเงินช่วยสมาชิกที่เกษียณv2.pdf' },
+    {
+      label: 'หนังสือสัญญากู้กองทุนสวัสดิการ',
+      path: '/assets/pdf/หนังสือสัญญากู้กองทุนสวัสดิการv2.pdf',
+    },
+    {
+      label: 'หนังสือค้ำประกันเงินกู้กองทุนสวัสดิการ',
+      path: '/assets/pdf/หนังสือค้ำประกันเงินกู้กองทุนสวัสดิการ.pdf',
+    },
+    {
+      label: 'หนังสือขอเปลี่ยนแปลงการใช้หุ้นค้ำประกัน',
+      path: '/assets/pdf/หนังสือขอเปลี่ยนแปลงการใช้หุ้นค้ำประกัน.pdf',
+    },
+    {
+      label: 'หนังสือขอเปลี่ยนผู้ค้ำประกัน',
+      path: '/assets/pdf/หนังสือขอเปลี่ยนผู้ค้ำประกัน.pdf',
+    },
+    {
+      label: 'หนังสือขอรับเงินช่วยสมาชิกที่เกษียณ',
+      path: '/assets/pdf/หนังสือขอรับเงินช่วยสมาชิกที่เกษียณv2.pdf',
+    },
     { label: 'ใบสำคัญรับเงิน', path: '/assets/pdf/ใบสำคัญรับเงิน.pdf' },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   initiateDownload(index: number): void {
     const documentPath = this.documents[index].path;
@@ -37,6 +58,4 @@ export class DepositComponentComponent implements OnInit {
     const pdfName = documentPath.label;
     saveAs(pdfPath, pdfName);
   }
-
 }
-
